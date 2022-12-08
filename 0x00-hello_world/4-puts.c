04c6997ea83099e3e871e2fcd29529d132d20136
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-09 00:53:27 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * print a line using function puts
+ * prints using function puts
  *
  * return value 0
  */
-int main ()
+int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
+    puts("\"Programming is like building a multilingual puzzle\n");
+    return (0);
 }
