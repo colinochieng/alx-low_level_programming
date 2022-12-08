d336ebcc8efea6d55477ed7ebc6ee99db6e6a4e5
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-09 01:19:25 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main: prints using function puts
  *
- * return value 0
+ * Return value 0
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle\n");
+    puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
