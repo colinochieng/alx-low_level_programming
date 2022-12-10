51a641cb79c5846eec7f8f4ec4e11de5ca16eb16
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-11 01:30:49 +0300

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main -  prints the alphabet in lowercase,removing q and e
+*Return: returns 0
+*/
+int main(void)
+{
+char lower_case = 'a';
+for  (; lower_case <= 'z'; lower_case++)
+{
+if ( lower_case == 'e' || lower_case == 'q')
+    {
+    continue;
+    }
+putchar(lower_case);
+}
+putchar('\n');
+return (0);
+}
