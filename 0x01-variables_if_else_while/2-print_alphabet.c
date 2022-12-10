723ed3ca669b10789800143fce807418318d4c9f
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-11 00:20:46 +0300

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main -  prints the alphabet in lowercase, followed by a new line
+*Return: returns 0
+*/
+int main(void)
+{
+	char lower_case = 'a';
+    while (lower_case < 'z')
+        {
+            putchar(lower_case);
+            lower_case++;
+        }
+putchar('\n');
+}
