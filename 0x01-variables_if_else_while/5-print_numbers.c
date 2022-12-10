bc1c6492c0513f70b7f051e73d812f576263fd34
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-11 02:09:46 +0300

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main -  prints the alphabet in lowercase,removing q and e
+*Return: returns 0
+*/
+int main(void)
+{
+int digit = 0;
+while (digit < 10)
+{
+printf("%d", digit);
+digit++;
+}
+}
