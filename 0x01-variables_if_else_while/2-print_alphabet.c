d8a9e81f9e60d16c651ccbff6971326e71fe6a93
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-11 00:25:11 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,11 +5,12 @@
 */
 int main(void)
 {
-	char lower_case = 'a';
-    while (lower_case < 'z')
-        {
-            putchar(lower_case);
-            lower_case++;
-        }
+char lower_case = 'a';
+while (lower_case < 'z')
+{
+    putchar(lower_case);
+    lower_case++;
+}
 putchar('\n');
+return (0);
 }
