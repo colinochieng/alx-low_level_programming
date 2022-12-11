d84a7508d4f1f8902a33628e79c0142c77de8528
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-11 17:46:00 +0300

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - outputs  all possible combinations of single-digit numbers
+*Description: 'Use while and for loop'
+*Return: returns 0
+*/
+int main(void)
+{
+char digit = '0';
+while (digit <= '9')
+{
+for (;digit<= '8'; digit ++)
+{
+putchar(digit);
+putchar(44);
+putchar(32);
+}
+putchar(digit);
+digit++;
+}
+return (0);
+}
