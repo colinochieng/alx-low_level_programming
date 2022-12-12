df0bebbc5aabf5ba857b33b77adeeb657e2cfefc
colinochieng <colinokumu@gmail.com>
2022-12-12 04:39:33 -0800

    all possible combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+ * main - outputs combinations of two two-digit numbers
+ * Description : use while loop for times and four intergers
+ * Returns : 0
+ */
+int main(void)
+{
+	int i = '0';
+	int j, k, l;
+	while (i <= '9')
+	{
+		j = '0';
+		while (j <= '8')
+		{
+			k = i;
+			while (k <= '9')
+			{
+				l = j+1;
+				 while (l <= '9')
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (i == '9' && j == '8' && k == '9' && l == '9')
+					{
+						putchar('\n');
+					}
+					else
+					{
+						putchar(',');
+					}
+					putchar(' ');
+					l++;
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
