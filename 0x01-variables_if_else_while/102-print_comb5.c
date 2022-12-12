e61ae6118a5ff0f4231a27871ecbd35a13f1398c
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-12 20:41:53 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 /**
  * main - outputs combinations of two two-digit numbers
+ *
  * Description : 'use while loop for times and four intergers'
+ *
  * Returns : 0
  */
 int main(void)
 {
 	int i = '0';
 	int j, k, l;
-	while (i <= '9')
-		
+	while (i <= '9') 
 	{
 		j = '0';
 		while (j <= '9')
