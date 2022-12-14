501f59ad95ff571218e2f0037048006681e9e1d5
colinochieng <colinokumu@gmail.com>
2022-12-14 01:28:26 -0800

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - checks for value of a number
+ *
+ * Description: 'prints +, 0, and' -
+ *
+ * @n: placeholder
+ *
+ * Returns: 0 1 or -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
