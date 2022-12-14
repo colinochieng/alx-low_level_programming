d82c11943e31eadfcd843d78869f49be291361d0
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-14 13:22:41 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,13 +2,13 @@
 /**
  *  _abs - computes the absolute value of an integer
  *
- * @int: parameter type
+ * @number: parameter type
  *  Return: number or 0
  */
 int _abs(int number)
 {
-       	if (number > 0)
-		return (number);
-	else
-		return (-number);
+if (number > 0)
+	return (number);
+else
+	return (-number);
 }
