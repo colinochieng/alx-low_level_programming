60120841a6f5570a3d01b8c6bd12897f0f4b86ea
colinochieng <colinokumu@gmail.com>
2022-12-14 16:23:26 -0800

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - prints sum
+ *
+ * @a: holds space for first number
+ * @b: holds space for second number
+ *
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
