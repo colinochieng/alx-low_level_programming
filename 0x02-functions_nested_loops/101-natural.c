aa8514cc3207d9051aba006856d106440dcc1d82
colinochieng <colinokumu@gmail.com>
2022-12-15 12:02:26 -0800

    sum of numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - adds all multiples of 3 and 5 between 0 and 1024
+ *
+ * Description: 'Use while loop and nested ifstatements'
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int value = 0;
+	int value = 0;
+
+	while (value <= 1024)
+	{
+		if (value > 0)
+		{
+			if (value % 5 ==0 || value % 3 == 0)
+			{
+				sum = value + sum;
+			}
+		}
+		value++;
+	}
+	putchar((sum / 100000) + '0');
+	putchar(((sum / 10000) % 10) + '0');
+	putchar(((sum / 1000) % 10) + '0');
+	putchar(((sum / 100) % 10) + '0');
+	putchar(((sum / 10) % 10) + '0');
+	putchar((sum % 10) + '0');
+	putchar('\n');
+
+	return (0);
+}
