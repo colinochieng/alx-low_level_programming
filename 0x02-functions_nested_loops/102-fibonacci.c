b2d601fa63973365773ed052adf7073e3ed9b92e
colinochieng <colinokumu@gmail.com>
2022-12-15 14:22:30 -0800

    first 50 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints 50 Fibonacci numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+unsigned long  first_num, second_num, value;
+int rate = 0;
+
+first_num = 0;
+second_num = 1;
+
+while (rate <= 4)
+{
+value = first_num + second_num;
+printf("%lu", value);
+first_num = second_num;
+second_num = value;
+if (rate < 49)
+{
+putchar(',');
+putchar(' ');
+}
+else
+
+putchar('\n');
+
+rate++;
+}
+}
+
