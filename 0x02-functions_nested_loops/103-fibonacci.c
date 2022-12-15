3af93df23877e4da96809fd3bf2501f3fd95b853
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-16 01:47:01 +0300

    Create 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+*main - print sum of even fibonacci < 4000000
+*Return: (0)
+*/
+
+int main(void)
+{
+int  rate;
+unsigned long  first_num, second_num, value, sum;
+rate = 0;
+first_num = 0;
+second_num = 1;
+sum = 0;
+
+while (rate <= 49)
+{
+value = first_num + second_num;
+first_num = second_num;
+second_num = value;
+
+if (value <= 4000000)
+{
+if (value%2 == 0)
+{
+    sum = value + sum;
+}
+}
+
+rate++;
+}
+printf("%lu", sum);
+
+return (0);
+}
