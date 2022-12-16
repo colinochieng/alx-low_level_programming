e3e4744e0b5ab21f4afc056a71de0964519a17c1
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-16 23:49:18 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,17 +13,14 @@ while (i <= 100)
 if (i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz");
-putchar(' ');
 }
 else if (i % 3 == 0)
 {
 printf("Fizz");
-putchar(' ');
 }
 else if (i % 5 == 0)
 {
 printf("Buzz");
-putchar(' ');
 }
 
 else
