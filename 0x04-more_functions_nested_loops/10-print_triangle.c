839bedb5d9894c7b74f1e8793aac2de1b1e79a43
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-17 11:18:12 +0300

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,6 +8,8 @@
 void print_triangle(int size)
 {
 int i, j;
+if (n > 0)
+{
 for (i = 1; i <= size; i++)
 {
 for (j = 1; j <= size - i; j++)
@@ -20,4 +22,7 @@ _putchar('#');
 }
 _putchar('\n');
 }
+}
+else
+_putchar('\n'); 
 }
