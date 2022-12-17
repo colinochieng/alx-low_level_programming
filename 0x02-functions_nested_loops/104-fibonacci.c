b8c9c9ebbe4c26601540355adde9a81678df832b
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-17 22:53:23 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -16,17 +16,14 @@ while (rate < 92)
 {
 value = first_num + second_num;
 printf("%lu, ", value);
-
 first_num = second_num;
 second_num = value;
 rate++;
 }
-
 n1 = first_num / 10000000000;
 n3 = second_num / 10000000000;
 n2 = first_num % 10000000000;
 n4 = second_num % 10000000000;
-
 rate = 93;
 while (rate < 99)
 {
@@ -35,19 +32,16 @@ h2 = n2 + n4;
 if (n2 + n4 > 9999999999)
 {
 h1 = h1 + 1;
-h2 = h2 + 10000000000;
+h2 = h2 % 10000000000;
 }
-
 printf("%lu%lu", h1, h2);
 if (rate != 98)
 putchar(',');
 putchar(' ');
-
 n1 = n3;
 n2 = n4;
 n3 = h1;
 n4 = h2;
-
 rate++;
 }
 putchar('\n');
