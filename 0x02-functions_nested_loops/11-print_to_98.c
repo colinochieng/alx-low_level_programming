94ce8a4cecf553f30a087997b109319768156fd7
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-17 15:39:31 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 {
 if (n <= 98)
 {
-while(n<=98)
+while (n <= 98)
 {
 printf("%d", n);
 if (n < 98)
@@ -25,8 +25,7 @@ putchar('\n');
 }
 else if (n >= 98)
 {
-n >= 98;
-while (n >=98)
+while (n >= 98)
 {
 printf("%d", n);
 if (n > 98)
