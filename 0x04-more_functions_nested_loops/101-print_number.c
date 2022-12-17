f13ca2ca4c3d63452236c7ca0499ea0f603a4a13
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-17 13:04:49 +0300

    Create 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - outputs intergers
+ * @n:arguments place holder
+ *Return: no value
+ **/
+
+void print_number(int n)
+
+{
+	unsigned int interger = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		interger = -interger;
+	}
+
+	if (interger / 10)
+	{
+		print_number(interger / 10);
+	}
+
+	_putchar(interger % 10 + '0');
+
+}
