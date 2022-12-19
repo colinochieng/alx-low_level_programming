c80ad3d690e13c3947074e981ce4fefaf68ba74a
colinochieng <colinokumu@gmail.com>
2022-12-19 13:32:03 -0800

    Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - changes the value of teo intergers
+ *
+ * @a: first argument pointer
+ * @b: second argument pointer
+ *
+ * Return: no value
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,4 +4,9 @@
 
 void reset_to_98(int *n);
 int _putchar(char c);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
 #endif /*MAIN_H*/
