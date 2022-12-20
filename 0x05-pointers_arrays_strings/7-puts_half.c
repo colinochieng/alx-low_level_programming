a543ad1a1b9ab88335ba928d472b825f239c2049
colinochieng <colinokumu@gmail.com>
2022-12-20 03:26:15 -0800

    push

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,11 +13,29 @@ void puts_half(char *str)
 int i = 0;
 int rev = strlen(str);
 
+if (rev % 2 == 0)
+{
 i = rev / 2;
 
 while (str[i] != '\0')
 {
 putchar(str[i]);
 i++;
+}
+}
+
+if (rev % 2 == 1)
+{
+i = (rev / 2) + 1;
+
+while (str[i] != '\0')
+{
+putchar(str[i]);
+i++;
+}
+}
+else if (str[i] == '\0')
+{
+_putchar('\n');
 }
 }
