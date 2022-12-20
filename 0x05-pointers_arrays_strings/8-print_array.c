20aeda540ee3bf0c9eef1a9efea38359fb1810c5
colinochieng <colinokumu@gmail.com>
2022-12-20 03:57:33 -0800

    elements of an array of integers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_array - prints array of intergers
+ *
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: no value
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i <= n - 2)
+			printf("%d, ", a[i]);
+		if (i == n - 1)
+			printf("%d", a[i]);
+
+		i++;
+	}
+
+	printf("\n");
+}
