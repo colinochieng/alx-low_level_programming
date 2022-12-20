e59da853869d6f6dec5d0034aa69cf5a4a29dcfb
colinochieng <colinokumu@gmail.com>
2022-12-19 23:10:51 -0800

    trial

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,12 +10,14 @@
 void rev_string(char *s)
 {
 int rev = strlen(s);
-int j = rev - 1;
-
-while (j >= 0)
+int j = 0;
+char i;
+while (j < rev / 2)
 {
-_putchar(s[j]);
-j--;
+i = s[j];
+s[j] = s[rev - j - 1];
+s[rev - j - 1] = i;
+j++;
 }
 _putchar('\n');
 }
