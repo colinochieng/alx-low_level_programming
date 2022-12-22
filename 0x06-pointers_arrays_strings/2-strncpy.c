b9722f426880e32cc21dda6a7e64bebc696171ae
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-23 01:51:10 +0300

    Create 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strncpy  - copies strings
+*
+* @dest: first pointer 
+* @src: second pointer
+* @n: len
+* Return: dest (copied string)
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[j] = src[j];
+j++;
+}
+while (j < n)
+{
+dest[j] = '\0';
+j++;
+}
