6bee83268dd80d7cad7053bcd3a50da10c2600c0
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-23 23:53:42 +0300

    Create 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* leet - encodes a string into 1337
+*
+*@s: takes the value of passed string
+*
+*Return: s
+*/
+
+char *leet(char *s)
+{
+int a [11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+int b [11] = {'a', 'A', 'e', 'E', '0', 'O', 't', 'T', 'l', 'L'};
+int i, j;
+i = 0;
+
+while (s[j] != '\0')
+{
+j = 0;
+for (i = 0; a[i] != '\0'; i++)
+{
+if (s[j] == b[i])
+s[j] = a[i];
+}
+j++;
+}
+
+return(s);
+}
