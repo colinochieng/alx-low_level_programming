936ddc1e0ef4630193000b1fec86624fcd6e963c
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-23 12:56:24 +0300

    Create 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* print_number - prints an integer
+* @n:arguments place holder
+*Return: no value
+**/
+void print_number(int n)
+
+{
+int int_number = n;
+
+if (n < 0)
+{
+putchar('-');
+int_number = -int_number;
+}
+
+if(int_number / 10)
+{
+print_number(int_number / 10);
+}
+
+putchar((int_number % 10) + '0');
+
+}
