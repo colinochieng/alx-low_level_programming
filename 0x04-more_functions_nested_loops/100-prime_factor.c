a44ccaefa9e5fa6473c0f0c29549162f3269a3f2
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-27 19:54:21 +0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,6 +6,7 @@ int main() {
 
 int64_t num = 612852475143;
 int64_t largest_factor = 1;
+int64_t i;
 
 int64_t max_factor = (int64_t) sqrt(num);
 for (int64_t i = 2; i <= max_factor; i++) {
@@ -19,6 +20,6 @@ if (num > 1) {
 largest_factor = num;
 }
 
-printf("%lld\n", largest_factor);
+printf("%ld\n", largest_factor);
 return 0;
 }
