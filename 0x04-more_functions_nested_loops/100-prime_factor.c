53f022b7ac7e4258f2191cdcb0ddc9f800aa6b0e
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-27 19:59:41 +0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <math.h>
+/**
+*main - prints max prime factor
+*Return: 0
+*/
 
-int main() {
+int main(void)
+{
 
 int64_t num = 612852475143;
 int64_t largest_factor = 1;
 int64_t i;
 
 int64_t max_factor = (int64_t) sqrt(num);
-for (i = 2; i <= max_factor; i++) {
-while (num % i == 0) {
+for (i = 2; i <= max_factor; i++) 
+{
+while (num % i == 0)
+{
 largest_factor = i;
 num /= i;
 }
 }
 
-if (num > 1) {
+if (num > 1) 
 largest_factor = num;
-}
 
 printf("%ld\n", largest_factor);
-return 0;
+return (0);
 }
