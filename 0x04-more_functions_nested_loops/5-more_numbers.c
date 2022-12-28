954ff1ecd00f935d9c25ea37212e2b703e817ced
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-28 23:01:24 +0300

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+*void more_numbers - outputs ten times number from 0 to 14
+*Return: 0
+*/
+void more_numbers(void)
+{
+int j = 0;
+int i;
+while(j<= 10)
+{
+for (i = 0; i < 15; i++)
+{
+if (i > 9)
+{
+_putchar('1');
+_putchar(i % 10 + '0');
+} else {
+_putchar(i + '0');
+}
+_putchar(' ');
+}
+_putchar('\n');
+j++;
+}
+return (0);
+}
