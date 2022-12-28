f344174eb303758abe9f56cd2603d65c1175bb29
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-29 00:40:10 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,30 +5,29 @@
 *@s2: second parameter
 *Return: 0, 15 or -15
 */
-int _strcmp(const char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 while (*s1 != '\0' && *s2 != '\0')
 {
 if (*s1 < *s2)
 {
-return -15;
+return (-15);
 }
 else if (*s1 > *s2)
 {
-return 15;
+return (15);
 }
 s1++;
 s2++;
 }
 
 if (*s1 == '\0' && *s2 == '\0')
-{
-return 0;
-}
+return (0);
 else if (*s1 == '\0')
 {
-return -15;
-} else {
-return 15;
+return (-15);
+}
+else {
+return (15);
 }
 }
