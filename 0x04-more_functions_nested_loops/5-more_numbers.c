e59c47246f9551d97e5b0cb9f1f327d089b6e7b9
colinochieng <117777762+colinochieng@users.noreply.github.com>
2022-12-30 14:48:45 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,22 +7,16 @@ void more_numbers(void)
 {
 int j = 0;
 int i;
+
 while (j <= 10)
 {
 for (i = 0; i < 15; i++)
 {
 if (i > 9)
-{
-_putchar('1');
-_putchar(i % 10 + '0');
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
 }
-else
-{
-_putchar(i + '0');
-}
-_putchar(' ');
-}
-_putchar('\n');
+putchar('\n');
 j++;
 }
 }
