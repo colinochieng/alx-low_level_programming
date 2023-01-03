dbc8ce6619afce4e077fa06ca714b754cc933f22
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-03 09:27:08 +0300

    Create 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+*_memset - fuction that fills memeory with constant bytes
+*@s: pointer to the memory location
+*@b: contant byte to be filled into the memory
+*@n: number of b to filled
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+int i;
+
+for (i = 0; i < n ; i++)
+s[i] = b;
+return (s);
+}
