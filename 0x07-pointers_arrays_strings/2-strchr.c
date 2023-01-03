ed8f0dd287f88df0927c35e3c633e856d9735391
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-03 10:33:58 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,16 +2,17 @@
 /**
 *_strchr -  function that positions a character in a string
 *@s: pointer to a string
-@c: character to be positioned
+*@c: character to be positioned
 *Return: NULL or pointer to a string
 */
 char *_strchr(char *s, char c)
 {
-    int i = 0;
-    while (s[i] != c)
-        i++;
-    if (s[i] == c)
-        return (s + i);
-    else
-    return (NULL);
+int i = 0;
+
+while (s[i] != c)
+i++;
+if (s[i] == c)
+return (s + i);
+else
+return (NULL);
 }
