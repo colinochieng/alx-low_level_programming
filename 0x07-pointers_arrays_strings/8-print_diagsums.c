e9e81b2ca4deda1c192b40778f7fd525dc83479a
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-04 03:08:10 +0300

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,12 +9,12 @@
 void print_diagsums(int *a, int size)
 {
 int i, j;
-unsigned add1, add2;
+unsigned int add1, add2;
 
 i = 0;
 while (i < size)
 {
-j = (i * size) + i;
+j = (i *size) + i;
 add1 = add1 + a[j];
 i++;
 }
@@ -22,7 +22,10 @@ i++;
 i = 1;
 while (i < size + 1)
 {
-j = (i * size) - i;
+j = (i *size) - i;
 add2 = add2 + a[j];
 i++;
+}
+printf("%d", add1);
+printf(", %d\n", add2);
 }
