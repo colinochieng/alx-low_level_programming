b82710a443b7b5a15ef1eacb76117859fe71229f
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-11 00:24:03 +0300

    Create 6-abs.c

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *  _abs - computes the absolute value of an integer
+ *
+ * @number: parameter type
+ *  Return: number or 0
+ */
+int _abs(int number)
+{
+if (number > 0)
+	return (number);
+else
+	return (-number);
+}
