0a7f05ffa68e3aa31493f8eefb09cc19811ff457
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-11 00:22:12 +0300

    Create 4-isalpha.c

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - checks of alphabets
+ *
+ * Description: 'use if else statements
+ *
+ * @c: paramater holding alphabets
+ *
+ * Return: 0 or 1
+ */
+int _isalpha(int c)
+{
+	if (!(c >= 'a' && c <= 'z') && !(c >= 'A' && c <= 'Z'))
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
