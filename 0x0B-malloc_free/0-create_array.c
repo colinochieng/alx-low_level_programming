11815cc4f8d5dd8593ff363943a79fbba898ba6c
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-12 23:25:36 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,12 +16,13 @@ char *create_array(unsigned int size, char c)
 
 
 	if (size == 0)
-		return (0);
+		return (NULL);
 	
 	ch_ptr = malloc(sizeof(char) * size);
 	if (ch_ptr == 0)
-		return (0);
+		return (NULL);
 
+	iter = 0;
 	while (iter < size)
 	{
 		ch_ptr[iter] = c;
