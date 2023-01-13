d4f0da6cb6fbdc6ec8aca5fefb8c306e760acf14
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-14 01:20:39 +0300

    Create 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+char *_memset(char *s, char b, unsigned int n);
+
+/**
+ * _calloc - a function that allocates memory for an array
+ *uses malloc
+ * @nmemb: size of array
+ * @size: size per element in bytes
+ * Return: memory pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *s;
+	int mem_size = size * nmemb;
+
+	if (!nmemb || !size)
+		return (NULL);
+  
+	s = malloc(mem_size);
+	if (!s)
+		return (NULL);
+  
+	_memset(s, 0, mem_size);
+ 
+  	return (s);
+}
+
+/**
+*_memset - fuction that fills memeory with constant bytes
+*@s: pointer to the memory location
+*@b: contant byte to be filled into the memory
+*@n: number of b to filled
+*Return: s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int recur;
+
+for (recur = 0; recur < n ; recur++)
+s[recur] = b;
+return (s);
+}
