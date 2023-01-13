ed7f29a32b398ec81d252c303754a27ed30175e6
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-14 01:50:14 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,45 +1,45 @@
-#include "main.h"
-#include <stdlib.h>
+	#include "main.h"
+	#include <stdlib.h>
 
-char *_memset(char *s, char b, unsigned int n);
+	char *_memset(char *s, char b, unsigned int n);
 
-/**
- * _calloc - a function that allocates memory for an array
- *uses malloc
- * @nmemb: size of array
- * @size: size per element in bytes
- * Return: memory pointer
- */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
+	/**
+	* _calloc - a function that allocates memory for an array
+	*uses malloc
+	* @nmemb: size of array
+	* @size: size per element in bytes
+	* Return: memory pointer
+	*/
+	void *_calloc(unsigned int nmemb, unsigned int size)
+	{
 	char *s;
 	int mem_size = size * nmemb;
 
 	if (!nmemb || !size)
-		return (NULL);
+	return (NULL);
 
 	s = malloc(mem_size);
 	if (!s)
-		return (NULL);
+	return (NULL);
 
 	_memset(s, 0, mem_size);
 
-  	return (s);
-}
+	return (s);
+	}
 
-/**
-*_memset - fuction that fills memeory with constant bytes
-*@s: pointer to the memory location
-*@b: contant byte to be filled into the memory
-*@n: number of b to filled
-*Return: s
-*/
+	/**
+	*_memset - fuction that fills memeory with constant bytes
+	*@s: pointer to the memory location
+	*@b: contant byte to be filled into the memory
+	*@n: number of b to filled
+	*Return: s
+	*/
 
-char *_memset(char *s, char b, unsigned int n)
-{
-unsigned int recur;
+	char *_memset(char *s, char b, unsigned int n)
+	{
+	unsigned int recur;
 
-for (recur = 0; recur < n ; recur++)
-s[recur] = b;
-return (s);
-}
+	for (recur = 0; recur < n ; recur++)
+	s[recur] = b;
+	return (s);
+	}
