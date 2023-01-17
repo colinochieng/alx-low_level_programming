b806e2a2e8091f4f048dccea173cec57b2693148
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-18 00:10:45 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,9 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+/**
+*dog_t - synonym for struct dog
+*/
+typedef struct dog dog_t;
+
 #endif /*DOG_H*/
