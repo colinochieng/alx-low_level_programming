f4806ccdd9980b5fbc70b4d27093cf0aeeb26c39
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-20 01:36:28 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -47,8 +47,7 @@ int op_div(int a, int b)
 {
 	if (b == 0) 
 	{
-		printf("Error\n");
-		exit (100);
+		return (0);
 	}
 	return (a / b);
 }
@@ -64,8 +63,7 @@ int op_mod(int a, int b)
 {
 	if (b == 0)
 	{
-		printf("Error\n");
-		exit (100);
+		return (0);
 	}
 	return (a % b);
 }
