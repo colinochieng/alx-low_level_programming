5d6fdf46fd0a4f77ab9134a9161d852878e56026
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-20 01:46:31 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -7,24 +7,24 @@
 */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-        	{"+", op_add},
-        	{"-", op_sub},
-        	{"*", op_mul},
-        	{"/", op_div},
-        	{"%", op_mod},
-        	{NULL, NULL}
-	};
-	int i;
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i;
 
-	i = 0;
-    	while (ops[i].op)
-    	{
-        	if (*s == *(ops[i].op))
-        	{
-            		return (ops[i].f);
-        	}
-        	i++;
-   	 }
-   	 return (NULL);
+i = 0;
+while (ops[i].op)
+{
+if (*s == *(ops[i].op))
+{
+return (ops[i].f);
+}
+i++;
+}
+return (NULL);
 }
