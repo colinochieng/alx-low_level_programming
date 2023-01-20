6bc6f1a5d5dcb0bb3b1ff99756b6a1e0eb95ef57
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-20 16:25:13 +0300

    Create 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* cap_string - capitalizes all words of a string
+*checks on word separators
+*s: pointer to string
+*Return: capitalized words in string
+*/
+char *cap_string(char *str)
+{
+int i, j;
+/* Declaring a string of word separators*/
+char punc[13] = {' ', ',', '.','"','?', '(','(', '\t','\n', '{', '}', ';'};
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[0] >= 97 && str[0] <= 122)
+str[0] -= 32;
+
+j = 0;
+
+while (j < 13)
+{
+if (str[i] == punc[j])
+{
+if(str[i + 1] >= 97 && str[i + 1] <= 122)
+str[i + 1] -= 32;
+}
+j++;
+}
+}
+
+return (str);
+}
