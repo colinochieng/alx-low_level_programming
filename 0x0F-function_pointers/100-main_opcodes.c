2f92b58526605dcf9494ff25808d8db23f90052d
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-22 23:29:08 +0300

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
 *dis_opcodes - dispaly opcodes of program
@@ -7,41 +9,41 @@
 */
 void dis_opcodes(char *a, int n)
 {
-	int i = 0;
+int i = 0;
 
-	while (i < n)
-    {
-        printf("%.2hhx", a[i]);
-		if (i < n - 1)
-			printf(" ");
-			i++;
-    }
+while (i < n)
+{
+printf("%.2hhx", a[i]);
+if (i < n - 1)
+printf(" ");
+i++;
+}
 
-	printf("\n");
+printf("\n");
 
 }
 
 /**
- * main - prints the opcodes
- * @argc: number of arguments passed
- * @argv: array of arguments
- * Return: O
- */
+* main - prints the opcodes
+* @argc: number of arguments passed
+* @argv: array of arguments
+* Return: O
+*/
 int main(int argc, char *argv[])
 {
-	int i;
+int i;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-	i = atoi(argv[1]);
-	if (i < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-	dis_opcodes((char *)&main, n);
-	return (0);
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+i = atoi(argv[1]);
+if (i < 0)
+{
+printf("Error\n");
+exit(2);
+}
+dis_opcodes((char *)&main, i);
+return (0);
 }
