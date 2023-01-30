1499e5b173b09a349b3820f00df6623362378873
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-30 18:33:12 +0300

    Create 0-print_list.c

---
+++ 0-print_list.c/0-print_list.c
@@ -0,0 +1,32 @@
+#include "list.h"
+
+/**
+*print_list - function that prints all the elements of a list_t list
+*If str is NULL, print [0] (nil)
+*@h: pointer to a list in struct
+*Return: the number of nodes(i)
+*/
+
+size_t print_list(const list_t *h)
+{
+	list_t *ptr;
+	ptr = h;
+	size_t i = 0;
+
+	while (ptr != NULL)
+	{
+		if (ptr->str == NULL && ptr->next != NULL)
+		{
+			ptr->str = "(nil)";
+			ptr->len = 0;
+		}
+
+		printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+		ptr = h;
+		i++;
+	}
+
+	return (i);
+}
