66ef594702d9f7294da1d20892bcc3dce0391249
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-30 20:19:18 +0300

    Create 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+*list_len - function prints number of elements in a linked list_t list
+*@h: pointer to a struct to make a linked list
+*Return: returns the number of elements in a linked list_t list(i)
+*/
+
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+	list_t *ptr;
+
+	ptr = (list_t *) h;
+
+	while (ptr != NULL)
+	{
+		
+		ptr = ptr->next;
+		i++;
+	}
+
+	return (i);
+}
