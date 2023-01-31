d18177a71f6265478fa9d07b86c00510bd48fc0d
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-01-31 17:53:07 +0300

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,13 @@
-section .data
-    hello db 'Hello, Holberton', 0
+section     .text
+    global      main
 
-section .text
-    global _start
+main:
+    mov     edx,len
+    mov     ecx,msg
+    mov     ebx,1
+    mov     eax,4
+    int     0x80
 
-_start:
-    ; printf("Hello, Holberton\n")
-    mov edx, 13
-    mov ecx, hello
-    mov ebx, 1
-    mov eax, 4
-    int 0x80
-
-    ; exit(0)
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
+section     .data
+msg     db  'Hello, Holberton',0xa
+	len     equ $ - msg
