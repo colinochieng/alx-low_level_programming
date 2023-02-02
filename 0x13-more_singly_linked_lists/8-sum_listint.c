b99bdc5222a0f1411eebcc102615359b591766bb
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-02-02 03:41:50 +0300

    Create 8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "list.h"
+
+/**
+*sum_listint - find the sum of all the data (n) of a listint_t linked list
+*@head: pointer listint_t linked list
+* Return: the sum
+*/
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *traverse;
+
+	traverse = head;
+
+	while (traverse != NULL)
+	{
+		sum += traverse->n;
+		traverse = traverse->next;
+	}
+
+	return (sum);
+}
