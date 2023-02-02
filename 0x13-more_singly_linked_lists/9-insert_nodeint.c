cb115e8b787b08e4744d5b1f08cc646367924571
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-02-03 01:06:17 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,6 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
         if (*head == NULL)
                 return (NULL);
+        
         if (idx == 0)
         {
                 insertint = *head;
