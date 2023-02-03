ced276e34a391d0c8abc2a4629a88721c5b23897
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-02-03 20:59:25 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -27,14 +27,14 @@ char *argstostr(int ac, char **av)
 
 	if (args_concat != NULL)
 	{
-	for (k = 0; k < j; k++)
-		{
-			for (i = 0; av[k][i] != '\0'; i++)
-				args_concat[i + value] = av[k][i];
-			args_concat[value + i] = '\n';
-			value += (i + 1);
-		}
-		args_concat[value] = '\0';
+		for (k = 0; k < j; k++)
+			{
+				for (i = 0; av[k][i] != '\0'; i++)
+					args_concat[i + value] = av[k][i];
+				args_concat[value + i] = '\n';
+				value += (i + 1);
+			}
+			args_concat[value] = '\0';
 	}
 	else
 	{
