8d27da1b12798538f197ceef453a1f0e55c41d45
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-02-03 21:00:13 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,35 +12,35 @@ int _strlen_recursion(char *s);
 
 char *argstostr(int ac, char **av)
 {
-	char *args_concat;
-	int i, j, k, len, value;
+char *args_concat;
+int i, j, k, len, value;
 
-	i = k = len = value = 0;
-	j = ac;
+i = k = len = value = 0;
+j = ac;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
+if (ac == 0 || av == NULL)
+return (NULL);
 
-	while (ac--)
-		len += (_strlen_recursion(av[ac]) + 1);
-	args_concat = (char *) malloc(len + 1);
+while (ac--)
+len += (_strlen_recursion(av[ac]) + 1);
+args_concat = (char *) malloc(len + 1);
 
-	if (args_concat != NULL)
-	{
-		for (k = 0; k < j; k++)
-			{
-				for (i = 0; av[k][i] != '\0'; i++)
-					args_concat[i + value] = av[k][i];
-				args_concat[value + i] = '\n';
-				value += (i + 1);
-			}
-			args_concat[value] = '\0';
-	}
-	else
-	{
-		return (NULL);
-	}
-	return (args_concat);
+if (args_concat != NULL)
+{
+for (k = 0; k < j; k++)
+{
+for (i = 0; av[k][i] != '\0'; i++)
+args_concat[i + value] = av[k][i];
+args_concat[value + i] = '\n';
+value += (i + 1);
+}
+args_concat[value] = '\0';
+}
+else
+{
+return (NULL);
+}
+return (args_concat);
 }
 
 /**
