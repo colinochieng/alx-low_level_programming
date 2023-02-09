b7eb916e75dd013cde0ab23a346c55f6c12ad68d
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-02-09 12:30:38 +0300

    Create 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+*read_textfile - reads a text file and prints it to the POSIX standard output
+*@filename: name of file to be read
+*@letters:  number of letters it should read and print
+*Return: the actual number of letters it could read and print
+*if the file can not be opened or read, return 0
+*if filename is NULL return 0
+*if write fails or does not write the expected amount of bytes, return 0
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t count, verify, print;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+
+	if (buf == NULL)
+		return (NULL);
+
+	verify = open(filename, O_RDONLY);
+
+	count = read(verify, buf, letters);
+	print = write(1, buf, count);
+
+	if (verify == -1 || count == -1 || print == -1 || print != count)
+		return (0);
+
+	return (print);
+}
