8ba487f626fc9f10a1ab1944827d7747bd2e9b36
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-02-10 14:21:06 +0300

    Create 101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,69 @@
+#include "main.h"
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+/**
+*Return: NULL if str == NULL or str == ""
+*/
+
+char** strtow(char *str)
+{
+	int count = 0;
+	int i, j;
+	int start = 0;
+	int end;
+
+    	if (str == NULL || str[0] == '\0')
+		return NULL;
+    
+/*counting no of space to determine number of words*/
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] == ' ')
+			count++;
+	}
+
+	char **result = (char**) malloc((count + 1) * sizeof(char*));
+
+	j = 0;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] == ' ' || str[i + 1] == '\0')
+		{
+			end = i;
+
+			if (str[i + 1] == '\0')
+			{
+				end = i + 1;
+			}
+
+			result[j] = (char*) malloc((end - start + 1) * sizeof(char));
+			_memcpy(result[j], &str[start], end - start);
+			result[j][end - start] = '\0';
+			j++;
+			start = i + 1;
+		}
+	}mam is coming
+
+    result[j] = NULL;
+
+    return result;
+}
+/**
+*_memcpy - function that copies memory location
+*@dest: pointer for for copied memory area
+*@src: pointer assigned memory area
+*@n: number of bytes to pe copied
+*Return: dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+dest[i] = src[i];
+
+return (dest);
+}
