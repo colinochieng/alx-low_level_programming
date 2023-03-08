5a39e37e547f53045cb908f807d13178938c60b8
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-03-08 17:32:13 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,21 +29,18 @@ int main(int argc, char *argv[])
 	int file_from, file_to, note, scan;
 	char buf[1024];
 
-/*if the number of argument is not the correct one*/
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	file_from = open(argv[1], O_RDONLY);
-/*if file_from does not exist*/
+
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-/*if file_to already exists, truncate it*/
 	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 /*if you can not create file_to*/
 	if (file_to == -1)
@@ -52,21 +49,22 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	scan = read(file_from, buf, 1024);
-
-	while (scan > 0)
+	while ((scan = read(file_from, buf, 1024)) > 0)
 	{
 		note = write(file_to, buf, scan);
-		/*if write to file_to fails*/
 		if (note != scan)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
-
+	
+	if (scan < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	close_file(file_from);
 	close_file(file_to);
-
 	return (0);
 }
