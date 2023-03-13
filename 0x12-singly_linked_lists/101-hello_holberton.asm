5a4470c267f0fd6765a7d09f815ce6a84927fd67
colinochieng <colinokumu@gmail.com>
2023-03-13 00:05:43 -0700

    Assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,22 @@
-section	.text
-	global _start     
-_start:           
-	mov	edx, len 
-	mov	ecx, msg
-	mov	ebx, 1
-	mov	eax, 4
-	int	0x80
-	mov	eax, 1
-	int	0x80
+; Declare needed C  functions
+	extern	printf		; the C function, to be called
 
-section	.data
+	section .data		; Data section, initialized variables
+msg:	db "Hello, Holberton", 0; C string needs 0
+fmt:	db "%s", 10, 0		; The printf format, "\n",'0'
 
-msg	db	'Hello, Holberton',0xa
-len	equ	$ - msg
+	section .text		; Code section.
+
+	global main		; the standard gcc entry point
+main:				; the program label for the entry point
+	push	rbp		; set up stack frame, must be alligned
+
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0		; or can be  xor  rax,rax
+	call	printf		; Call C function
+
+	pop	rbp		; restore stack
+
+	mov	rax,0		; normal, no error, return value
+	ret			; return
