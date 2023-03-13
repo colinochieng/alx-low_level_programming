357abeb33827df1bf3274f49c46a5bf148171616
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-03-13 08:25:47 +0300

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,15 +7,13 @@
 */
 void free_list(list_t *head)
 {
-        list_t *ptr, *temp;
-        
-        ptr = head;
-        while (ptr->next != NULL)
-        {
-                temp = ptr;
-                ptr = ptr->next;
-                free(temp->str);
-                free(temp);
-        }
-        head = NULL;
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
 }
