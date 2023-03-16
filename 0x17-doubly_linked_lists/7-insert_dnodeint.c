9ca7de6d75ec6aed3904ece8555be915a84d4794
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-03-16 23:42:58 +0300

    Create  7-insert_dnodeint.c

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+*insert_dnodeint_at_index - a function that inserts
+*a new node at a given position
+*@h: pointer to head of list
+*@idx: the index of the list where
+*the new node should be added. Index starts at 0
+*@n: integer for the new node
+*Returns: the address of the new node, or NULL if it failed
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *ptr = *h;
+	dlistint_t *temp2, *temp = malloc(sizeof(dlistint_t));
+
+	temp->n = n;
+	temp->next = NULL;
+	temp->prev = NULL;
+
+	while (idx != 1 && ptr != NULL)
+	{
+		ptr = ptr->next;
+		idx--;
+	}
+
+	if (idx != 1)
+		return (NULL);
+
+	if (ptr->next == NULL)
+	{
+		ptr->next = temp;
+		temp->prev = ptr;
+	}
+
+	else
+	{
+		temp2 = ptr->next;
+		ptr->next = temp;
+		temp2->prev = temp;
+		temp->next = temp2;
+		temp->prev = ptr;
+	}
+
+	return (*h);
+}
