c1a21ac0a5aadc2ffb3d0a4d381483a2ffddd7e4
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-03-16 18:05:11 +0300

    Create 1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len -  finds  the number of elements
+ *in a linked dlistint_t list
+ *@h: pointer to dlistint_t list
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	dlistint_t *ptr = (dlistint_t *) h;
+	size_t i = 0;
+
+	while (ptr != NULL)
+	{
+		i++;
+		ptr = ptr->next;
+	}
+	return(i);
+}
