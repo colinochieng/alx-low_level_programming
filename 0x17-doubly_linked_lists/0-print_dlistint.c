c6a9c64163770048d2dd64e93679fe555ac31608
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-03-16 17:59:26 +0300

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,15 +7,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-        dlistint_t *ptr = (dlistint_t *) h;
-        size_t i = 0;
+	dlistint_t *ptr = (dlistint_t *) h;
+	size_t i = 0;
 
-        while (ptr != NULL)
-        {
-                printf("%d\n", ptr->n);
-                i++;
-                ptr = ptr->next;
-        }
-        return(i);
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		i++;
+		ptr = ptr->next;
+	}
+	return(i);
 }
-
