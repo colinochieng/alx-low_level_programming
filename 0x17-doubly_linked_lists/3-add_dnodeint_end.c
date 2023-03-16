1627706d4ed755806cd6a75fbef8b38fdbdf7098
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-03-16 19:20:07 +0300

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -20,7 +20,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	if ((*head) == NULL)
 	{
-		ptr->next= NULL;
+		ptr->next = NULL;
 		ptr->prev = NULL;
 		*head = ptr;
 		return (*head);
