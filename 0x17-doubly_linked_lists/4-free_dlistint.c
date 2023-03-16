3e5fb5c5fcba5f85cbeb4155772d2b53f2d0b4ee
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-03-16 19:38:07 +0300

    Update 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -9,10 +9,13 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *ptr = head;
 
-	while (ptr->next != NULL)
+	if (ptr != NULL)
 	{
-		ptr = ptr->next;
-		free(ptr->prev);
+		while (ptr->next != NULL)
+		{
+			ptr = ptr->next;
+			free(ptr->prev);
+		}
+		free(ptr);
 	}
-	free(ptr);
 }
