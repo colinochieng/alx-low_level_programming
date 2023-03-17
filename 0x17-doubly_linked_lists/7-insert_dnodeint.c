ff55f33701c50ae7ce1c8c293244cffc32ca8652
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-03-17 15:34:35 +0300

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,6 +14,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *ptr = *h;
 	dlistint_t *temp2, *temp;
 
+	if (h == NULL)
+		return (NULL);
+
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
