c07aa337789616851ce995f13f8c758f557e4a1b
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-04-07 11:30:53 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,15 +17,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
-	{
-		free(table);
 		return (NULL);
-	}
 
 	/*Allocation of memory for hash_node_t pointers*/
 	table->array = malloc(sizeof(hash_node_t) * size);
 	if (table->array == NULL)
+	{
+		free(table);
 		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
