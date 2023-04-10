dcd65c6a1cd65368552dd12149609386edfdaf90
colinochieng <117777762+colinochieng@users.noreply.github.com>
2023-04-10 22:21:53 +0300

    Update 100-sorted_hash_table.c

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,26 +1,6 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
- *
- * Return: hash value
- */
-unsigned long int hash_djb2(const unsigned char *str)
-{
-	unsigned long int hash;
-	int c;
-
-	hash = 5381;
-	while ((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
-	return (hash);
-}
-
-
-/**
  * shash_table_create - creates the hash table of size 'size'
  * @size: size of new table
  * Return: new table or NULL
