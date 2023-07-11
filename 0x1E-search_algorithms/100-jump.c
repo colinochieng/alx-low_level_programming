db4be14eb9d598d18f3e23baef76e043cc490b69
colinochieng <colinokumu@gmail.com>
2023-07-11 08:14:47 -0700

    search alog

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -19,7 +19,7 @@ int jump_search(int *array, size_t size, int value)
 	if (!array || size == 0)
 		return (-1);
 
-	while (curr < size && array[curr] < value)
+	while (curr < size && array[curr] <= value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", curr, array[curr]);
 		prev = curr;
